e6fae73b57ec8769b9260547233b106e7539aec4
PavlovAlex <al3ksei.pav@yandex.ru>
2024-05-29 23:50:38 +0300

    файлы и разрешения

--- app/src/main/java/com/example/moviesapp/root/RootActivity.kt
+++ app/src/main/java/com/example/moviesapp/root/RootActivity.kt
@@ -2,14 +2,11 @@ package com.example.moviesapp.root
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.moviesapp.R
 import com.example.moviesapp.databinding.ActivityRootBinding
-import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class RootActivity : AppCompatActivity() {
 
@@ -32,59 +29,14 @@ class RootActivity : AppCompatActivity() {
                 R.id.detailsFragment, R.id.moviesCastFragment -> {
                     binding.bottomNavigationView.visibility = View.GONE
                 }
-
                 else -> {
                     binding.bottomNavigationView.visibility = View.VISIBLE
                 }
             }
         }
-
-        // выдвижное меню
-        val bottomSheetContainer = findViewById<LinearLayout>(R.id.standard_bottom_sheet)
-
-        //  BottomSheetBehavior.from() — вспомогательная функция, позволяющая получить объект BottomSheetBehavior, связанный с контейнером BottomSheet
-        val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheetContainer).apply {
-            state = BottomSheetBehavior.STATE_HIDDEN
-        }
-
-        findViewById<Button>(R.id.add).setOnClickListener {
-            bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
-        }
-
-        bottomSheetBehavior.addBottomSheetCallback(object :
-            BottomSheetBehavior.BottomSheetCallback() {
-            override fun onStateChanged(bottomSheet: View, newState: Int) {
-                // newState — новое состояние BottomSheet
-                when (newState) {
-                    BottomSheetBehavior.STATE_EXPANDED -> {
-                        // загружаем рекламный баннер
-                        //loadAds()
-                    }
-
-                    BottomSheetBehavior.STATE_COLLAPSED -> {
-                        // останавливаем трейлер
-                        //pauseTrailer()
-                    }
-
-                    BottomSheetBehavior.STATE_HIDDEN -> {
-                        // возобновляем трейлер
-                        // resumeTrailer()
-                    }
-
-                    else -> {
-                        // Остальные состояния не обрабатываем
-                    }
-                }
-            }
-
-            override fun onSlide(bottomSheet: View, slideOffset: Float) {}
-        })
     }
 
     fun animateBottomNavigationView() {
         binding.bottomNavigationView.visibility = View.GONE
     }
 }
-
-
-
